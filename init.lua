6e5dc1492aa9acb23ac04f693f70ba5270300954
Alex Olivares <alex.olivares@kibocommerce.com>
2022-09-20 17:09:42 -0500

    hammersoon initial config

---
+++ init.lua
@@ -0,0 +1,25 @@
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
+
+
+
+hs.hotkey.bind({"`", "cmd"}, "j", function() hs.application.launchOrFocus("Slack"); c() end)
+hs.hotkey.bind({"`", "cmd"}, "k", function() hs.application.launchOrFocus("Google Chrome"); c() end)
+hs.hotkey.bind({"`", "cmd"}, "h", function() hs.application.launchOrFocus("Microsoft Outlook"); c() end)
+hs.hotkey.bind({"`", "cmd"}, "f", function() hs.application.launchOrFocus("Finder"); c() end)
+hs.hotkey.bind({"`", "cmd"}, "l", function() hs.application.launchOrFocus("iTerm2"); c() end)
+hs.hotkey.bind({"`", "cmd"}, ";", function() hs.application.launchOrFocus("Visual Studio Code"); c() end)
+hs.hotkey.bind({"`", "cmd"}, "b", function() hs.application.launchOrFocus("Brave Browswer"); c() end)
+hs.hotkey.bind({"`", "cmd"}, "e", function() hs.application.launchOrFocus("Microsoft Excel"); c() end)
+hs.hotkey.bind({"`", "cmd"}, "p", function() hs.application.launchOrFocus("Postman"); c() end)
