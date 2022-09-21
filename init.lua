0a1c30d8d69d8313f050a40e526d3a0f3d36974f
Alex Olivares <alex.olivares@kibocommerce.com>
2022-09-21 16:06:08 -0500

    hotkeys

--- init.lua
+++ init.lua
@@ -15,7 +15,7 @@ hs.alert.show("Config loaded")
 
 
 hs.hotkey.bind({"`", "cmd"}, "j", function() hs.application.launchOrFocus("Slack"); c() end)
-hs.hotkey.bind({"`", "cmd"}, "k", function() hs.application.launchOrFocus("Google Chrome"); c() end)
+hs.hotkey.bind({"`", "cmd"}, "i", function() hs.application.launchOrFocus("Google Chrome"); c() end)
 hs.hotkey.bind({"`", "cmd"}, "h", function() hs.application.launchOrFocus("Microsoft Outlook"); c() end)
 hs.hotkey.bind({"`", "cmd"}, "u", function() hs.application.launchOrFocus("Finder"); c() end)
 hs.hotkey.bind({"`", "cmd"}, "l", function() hs.application.launchOrFocus("iTerm"); c() end)
@@ -24,4 +24,3 @@ hs.hotkey.bind({"`", "cmd"}, "b", function() hs.application.launchOrFocus("Brave
 hs.hotkey.bind({"`", "cmd"}, "e", function() hs.application.launchOrFocus("Microsoft Excel"); c() end)
 hs.hotkey.bind({"`", "cmd"}, "p", function() hs.application.launchOrFocus("Postman"); c() end)
 hs.hotkey.bind({"`", "cmd"}, "o", function() hs.application.launchOrFocus("Microsoft OneNote"); c() end)
-hs.hotkey.bind({"`", "cmd"}, "z", function() hs.application.launchOrFocus("Zoom.us"); c() end)
